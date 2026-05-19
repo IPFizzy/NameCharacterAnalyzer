22cc8841925e38674b7b027ea39f83af8cebe232
IPFizzy <fizzy@keonbushman.com>
2026-05-19 11:49:15 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleMessages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the GitHub Tutorial!");
+        }
+    }
+}
