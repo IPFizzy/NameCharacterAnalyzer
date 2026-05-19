82224e299d84c19c266d6393b6c2a228c84f0749
IPFizzy <fizzy@keonbushman.com>
2026-05-19 11:55:53 -0700

    Ask for name, say name

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace ConsoleMessages
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the GitHub Tutorial!");
+            Console.WriteLine("Please tell me your name");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello {0}, I am pleases to meet you", yourName);
+
+            Console.WriteLine("I")
         }
     }
 }
